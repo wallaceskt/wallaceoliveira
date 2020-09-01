1983de0b7a57667a371b366b4aa157487c19dc37
Wallace Oliveira <wallacesales@gmail.com>
2020-09-01 00:54:52 -0300

    Incluí um slide de imagens no portolio.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
-    //console.log( "document loaded" );
+    $(".images img:eq(0)").addClass("ativo").show();
+    setInterval(slide, 3000);
+
+    function slide() {
+        if($(".ativo").next().length) {
+            $(".ativo").fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+        } else {
+            $(".ativo").fadeOut().removeClass("ativo");
+            $(".images img:eq(0)").fadeIn().addClass("ativo");
+        }
+    }
 
     // Ir ao topo da página
     $("a.link-ancor-top").click(function() {
